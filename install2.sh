96036162fcafd031044b315cec68569072d285b9
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-04-09 13:19:54 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,12 +5,10 @@ echo
 
 
 cd
-cd ..
-cd usr/bin/
-cp -r ngrok $HOME/kali-fs/root
+
 echo tnx for contect to rajni
-cd
-rm -rf python-send-freesms
+mkdir kali-fs/root/.ngrok
+cd kali-fs/root/.ngrok2/
+nano ngrok.yml
 
 
-./start-kali.sh
