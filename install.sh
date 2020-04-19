cf7cc498d8a9fb6c788def0cb0639a983431174c
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-04-19 07:48:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 apt install w3m -y
 
+apt install python3  -y
+apt install python -y
+apt install python2  -y
+apt install python3-pip  -y
+mv pass.txt $HOME/Instagram/
+echo tnx
+
+
 open https://www.fast2sms.com
